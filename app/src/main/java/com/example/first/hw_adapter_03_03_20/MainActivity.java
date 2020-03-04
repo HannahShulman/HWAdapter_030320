53548ebb6068ea732e7dc63dabbd3fbd7f0463d6
hackdroid2020 <hackdroid2020@gmail.com>
2020-03-04 20:16:27 +0200

    Task 5: attach adapter to recycler view

--- app/src/main/java/com/example/first/hw_adapter_03_03_20/MainActivity.java
+++ app/src/main/java/com/example/first/hw_adapter_03_03_20/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.first.hw_adapter_03_03_20;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import com.example.first.hw_adapter_03_03_20.pojo.Person;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView personList = findViewById(R.id.person_list);
+
+        //set layoutmanager to set orientation of cells layout
+        personList.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+
+        ArrayList<Person> list = new ArrayList<Person>();
+        PersonAdapter adapter = new PersonAdapter(list);
+
+        //attach adapter to recyclerview
+        personList.setAdapter(adapter);
+
     }
 }
