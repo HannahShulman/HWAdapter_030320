64e699e3491e0701119de501d921382a5704b7d3
hackdroid2020 <hackdroid2020@gmail.com>
2020-03-04 20:35:19 +0200

    Task 6: fvbi and add contact

--- app/src/main/java/com/example/first/hw_adapter_03_03_20/MainActivity.java
+++ app/src/main/java/com/example/first/hw_adapter_03_03_20/MainActivity.java
@@ -5,12 +5,21 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.example.first.hw_adapter_03_03_20.pojo.Family;
+import com.example.first.hw_adapter_03_03_20.pojo.Friend;
 import com.example.first.hw_adapter_03_03_20.pojo.Person;
+import com.example.first.hw_adapter_03_03_20.pojo.Student;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText nameEt, phoneET, type_et;
+    Button addPerson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +32,39 @@ public class MainActivity extends AppCompatActivity {
         personList.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
 
         ArrayList<Person> list = new ArrayList<Person>();
-        PersonAdapter adapter = new PersonAdapter(list);
+        final PersonAdapter adapter = new PersonAdapter(list);
 
         //attach adapter to recyclerview
         personList.setAdapter(adapter);
+
+        nameEt = findViewById(R.id.name_et);
+        phoneET = findViewById(R.id.phone_et);
+        type_et = findViewById(R.id.type_et);
+        addPerson = findViewById(R.id.add_person);
+
+        addPerson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Person person;
+                switch (type_et.getText().toString().toLowerCase()){
+                    case  "s":
+                        person = new Student("Math", 59.0);
+                        break;
+                    case "f":
+                        person = new Friend();
+                        break;
+                    case "fa":
+                        person = new Family();
+                        break;
+                    default:
+                        person = new Friend();
+                }
+             person.setName(nameEt.getText().toString());
+             person.setPhone(phoneET.getText().toString());
+             adapter.addPerson(person);
+            }
+        });
+
 
     }
 }
