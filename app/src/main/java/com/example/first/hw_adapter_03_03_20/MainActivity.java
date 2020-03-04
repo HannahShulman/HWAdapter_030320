f2af27db550327899f08bded6f5f2f8931dd3f16
hackdroid2020 <hackdroid2020@gmail.com>
2020-03-04 18:53:31 +0200

    Task 3.MainActivity layout

---
+++ app/src/main/java/com/example/first/hw_adapter_03_03_20/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.first.hw_adapter_03_03_20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
