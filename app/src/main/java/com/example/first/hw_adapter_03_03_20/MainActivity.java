ea01600d8cb66dc20f43c6f7e5507e582fd57f76
Naftali <naftali@bk.ru>
2020-03-09 22:21:42 +0200

    * Edit main activity XML   - EditText moved to comments   - Gives ID for RadioGroup and RadioButtons * Edit main activity Java   - Add connection to Radio group   - Changed Switch

--- app/src/main/java/com/example/first/hw_adapter_03_03_20/MainActivity.java
+++ app/src/main/java/com/example/first/hw_adapter_03_03_20/MainActivity.java
@@ -8,6 +8,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.example.first.hw_adapter_03_03_20.pojo.Family;
 import com.example.first.hw_adapter_03_03_20.pojo.Friend;
@@ -39,21 +42,31 @@ public class MainActivity extends AppCompatActivity {
 
         nameEt = findViewById(R.id.name_et);
         phoneET = findViewById(R.id.phone_et);
-        type_et = findViewById(R.id.type_et);
+//        type_et = findViewById(R.id.type_et);
         addPerson = findViewById(R.id.add_person);
 
         addPerson.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Person person;
-                switch (type_et.getText().toString().toLowerCase()){
+
+                RadioGroup radioGroup = findViewById(R.id.type_radio); //taking group of radio
+                int selectedId = radioGroup.getCheckedRadioButtonId(); //checking, which one button was checked, getting id button (NOT id that you wrote in XML)
+                RadioButton radioButton = (RadioButton) findViewById(selectedId); //taking special button by id
+
+//                Toast.makeText(MainActivity.this, radioButton.getText().toString(), Toast.LENGTH_LONG).show(); //Testing code
+
+                switch (radioButton.getText().toString().toLowerCase()){
                     case  "s":
+                    case "student":
                         person = new Student("Math", 59.0);
                         break;
                     case "f":
+                    case "friend":
                         person = new Friend();
                         break;
                     case "fa":
+                    case "family":
                         person = new Family();
                         break;
                     default:
